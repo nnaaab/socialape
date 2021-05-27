66a138d34bbef43c680c380cf2d27f9b75a63e61
Ningning Zhu <nz14@rice.edu>
2021-05-27 14:15:06 -0500

    reorganize code

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDrkJIpJ_CgwWEgdV1jWeYVibZkV1Lljc0",
+  authDomain: "socialape-41329.firebaseapp.com",
+  projectId: "socialape-41329",
+  storageBucket: "socialape-41329.appspot.com",
+  messagingSenderId: "1041462219408",
+  appId: "1:1041462219408:web:7e5a57f2dd85173d5bfe15",
+  measurementId: "G-ZSDH4PJVTZ"
+};
